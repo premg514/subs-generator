05106f453cce64d4d05f59fbc9ddeb0a9d93b8f7
premg513 <gunduboinaprem@gmail.com>
2025-04-03 09:14:21 +0530

    launch

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
-
 import express from "express";
 import { configDotenv } from "dotenv";
 import fileUpload from "express-fileupload";
-import cors from "cors"
+import cors from "cors";
 import subRoutes from "./routes/subs.routes.js";
 
 const app = express();
 
-configDotenv();           //configure the env
-app.use(fileUpload());    //it will parse the mutipart data
-app.use(express.json());  // Enable JSON parsing for request bodies
-app.use(cors())           //configure cors
+configDotenv(); //configure the env
+app.use(fileUpload()); //it will parse the mutipart data
+app.use(express.json()); // Enable JSON parsing for request bodies
+const corsOptions = {
+  origin: "https://prems-subs-generator.vercel.app", // Allow only this frontend
+  methods: "GET,POST",
+  allowedHeaders: "Content-Type",
+};
 
-app.use("/api/subs",subRoutes);  // Use routes for the "/api/subs" endpoint
+app.use(cors(corsOptions)); //configure cors
 
-app.listen(process.env.PORT, () => {   //access the PORT from the .env
-  console.log("server started");         
+app.use("/api/subs", subRoutes); // Use routes for the "/api/subs" endpoint
+
+app.listen(process.env.PORT, () => {
+  //access the PORT from the .env
+  console.log("server started");
 });
